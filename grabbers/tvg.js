53d68032a4e4dc6dad7b25bd69c7160c7cb0ac96
axfree <axfree@live.com>
2019-05-02 16:22:31 +0900

    Fix tvG grabber

--- grabbers/tvg.js
+++ grabbers/tvg.js
@@ -87,78 +87,44 @@ function *grab(config, argv) {
                         evntCmpYmd: date.format('YYYYMMDD')
                     },
                 });
-                var body = iconv.decode(res.body, 'cp949');
+                var body = iconv.decode(res.body, 'cp949').replace(/<재>/g, `&lt;재&gt;`);
                 var nextLink = $('a.next', body);
-
                 var trs = $('tbody > tr', body);
                 trs.each((idx, tr) => {
                     var startTime = $('td:nth-child(1)', tr).text();    // 23:10
-                    var title = $('td:nth-child(2)', tr).text().trim();
+                    var title = $('td:nth-child(2)', tr).contents().first().text().trim();
+                    var rating = $('.cte_all', tr).text();
                     var program = {
                         start: moment(date.format('YYYYMMDD') + startTime + '+0900', 'YYYYMMDDHH:mmZ'),
                         category: $('td:nth-child(3)', tr).text().trim(),
-                        rating: 0
+                        rating: rating == 'All' ? 0 : +rating
                     }
 
-                    $('img', tr).each((idx, img) => {
-                        var alt = $(img).attr('alt');
-                        var m = alt.match(/(\d+)세이상 관람가/);
-                        if (m)
-                            program.rating = parseInt(m[1]);
-                    })
+                    // [카톨릭 명화극장 [교황 요한 23세 2-1]
+                    // <재> [카톨릭 명화극장 [교황 요한 23세 2-1]
+                    title = fixUnbalancedParenthesis(title, { removeUnbalancedOpener: true });
 
-                    // (생) [포르투갈:웨일스] UEFA 유로 2016 준결승
-                    // 국내특선다큐 [디지털 콘텐츠, 공짜와의 전쟁 2부]
-                    // 청년창업 RUNWAY [친환경 유아용품 전문 브랜드 최..
-                    // Smartest Guy In The Room (S1) (9,10회)
-                    // 방송 정보 없음
+                    // <재> TV 동물농장
+                    // <재> 녹두꽃 (1회)
+                    // 모닝와이드 (3부)
+                    // 강남스캔들 (111회)
+                    // 빅이슈 [최종] (32회)
+                    // [삼성:키움] 2019 프로야구
+                    // <재> 걸어서 세계속으로 [뉴욕-콘크리트 정글] (518회)
+                    // 녹두꽃 [3~4회, 조정석X윤시윤]      # \[(\d+)(?:~(\d+))?회(?:,.*)?\])?
+                    // <재> 아는 형님 [[직딩방학특집]김완선-바다-소유-케이] (177회)
+                    // <재> 연애의 참견 시즌2 (29회)
 
-                    // fix unbalanced parenthesis/brackets
-                    if (title.endsWith('..')) {
-                        var fixedTitle = '';
-                        var opens = [];
-                        var openers = '([{<';
-                        var closers = ')]}>';
-                        for (var c of title) {
-                            var idx = openers.indexOf(c);
-                            if (idx >= 0)
-                                opens.push(idx);
-                            else {
-                                idx = closers.indexOf(c);
-                                if (idx >= 0) {
-                                    if (opens.length == 0 || opens[opens.length - 1] != idx)
-                                        c = openers[idx] + c;
-                                    else
-                                        opens.pop();
-                                }
-                            }
-                            fixedTitle += c;
-                        }
-                        opens.reverse().forEach(idx => fixedTitle += closers[idx]);
-
-                        if (title != fixedTitle) {
-                            // console.log(`title changed: old=${title}, new=${fixedTitle}`);
-                            title = fixedTitle;
-                        }
-                    }
-
-                    //                    1                2              3:title        4                5
-                    var m = title.match(/^(\(생\))?(?:\s*\[(.*?)\])?(?:\s*(.*?))?(?:\s*\[(.*?)\])?(?:\s*\(([\d,]+회)\))?$/);
+                    //                    1              2              3:title        4                5
+                    var m = title.match(/^(<재>)?(?:\s*\[(.*?)\])?(?:\s*(.*?))?(?:\s*\[(.*?)\])?(?:\s*\((\d+회)\))?$/);
                     if (m) {
-                        if (m[3]) {
-                            program.title = m[3];
-                            program.subtitle = m[2] || m[4];
-                        }
-                        else
-                            program.title = m[2] || m[4];
-                        program.live = !!m[4];
+                        program.title = m[3];
+                        program.subtitle = m[2] || m[4];
+                        program.rebroadcast = !!m[1];
                         program.episode = m[5];
                     }
-
-                    // if (!program.title) {
-                    //     console.error(channelFullName, title, program.title);
-                    //     process.exit(0);
-                    // }
+                    else
+                        throw new Error(`title parse failed: ch=${channelName}, title="${title}"`);
 
                     programs.push(program);
                 });
@@ -176,6 +142,54 @@ function *grab(config, argv) {
     }
 
     return channels;
+}
+
+const openerSet = '([{<';
+const closerSet = ')]}>';
+
+function fixUnbalancedParenthesis(title, options) {
+    // options.prependMissingOpener
+    // options.removeUnbalancedOpener
+    var output = [];
+    var openers = [];
+    for (var c of title) {
+        var openerIdx = openerSet.indexOf(c);
+        if (openerIdx >= 0) {
+            openers.push(openerIdx);
+            output.push(c);
+        }
+        else {
+            var closerIdx = closerSet.indexOf(c);
+            if (closerIdx >= 0) {
+                var openerFound = false;
+                while (openers.length > 0) {
+                    var openerIdx = openers.pop();
+                    if (openerIdx == closerIdx) {
+                        output.push(c);
+                        openerFound = true;
+                        break;
+                    }
+                    output.push(closerSet[openerIdx])
+                }
+                if (!openerFound) {
+                    if (options.prependMissingOpener)
+                        output = [ openerSet[closerIdx], ...output, c ];
+                }
+            }
+            else
+                output.push(c);
+        }
+    }
+
+    if (options.removeUnbalancedOpener) {
+        openers.forEach(idx => {
+            output.splice(output.indexOf(openerSet[idx]), 1);
+        });
+    }
+    else
+        openers.reverse().forEach(idx => output.push(closerSet[idx]));
+
+    return output.join('');
 }
 
 module.exports = {
